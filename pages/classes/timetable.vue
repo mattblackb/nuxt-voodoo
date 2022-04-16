21abec7dd72e731ccc3acadd593610db42bb7fdf
Matt Burton <matt@plain.black>
2022-04-16 11:44:17 +0000

    Timetable updated

--- pages/classes/timetable.vue
+++ pages/classes/timetable.vue
@@ -10,7 +10,23 @@
       </PageHeader>
         <MainView linkType="classes" >
                  <p>Timetable for Voodoo Combat; Find out exactly when every Martial Art class, training course, development opportunity and social event takes place throughout the year and never miss one again. Click on an event and see more detail or even copy it directly to your calendar.</p>
-    <iframe style="border-width: 0;" src="https://calendar.google.com/calendar/embed?mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=webape2%40gmail.com&amp;color=%2329527A&amp;ctz=Europe%2FLondon" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+                       <a href="/Timetables.pdf" target="_blank">
+                        <img src="~/static/timetable.png">
+                        </a>
+                       
+                      </nuxt-link>
+                      <v-row class="mt-4 mb-4">
+ <v-btn
+        outlined
+        rounded
+        text
+        target="_blank"
+        href= "/Timetables.pdf"
+      >
+        View as PDF
+      </v-btn>
+      </v-row>
+    <iframe style="border-width: 0;" src="https://calendar.google.com/calendar/embed?mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=webape2%40gmail.com&amp;color=%2329527A&amp;ctz=Europe%2FLondon" width="760" height="600" frameborder="0" scrolling="no"></iframe>
 
 
         </MainView>
@@ -22,3 +38,6 @@
   </v-container>
 </template>
 
+<style scoped>
+ img { max-width: 100%}
+</style>
