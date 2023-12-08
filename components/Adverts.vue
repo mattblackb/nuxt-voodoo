ed26478b1ece19fe1b96afca0cd8ea67732ba5a9
mattblackb <87385656+mattblackb@users.noreply.github.com>
2023-12-08 21:54:05 +0000

    Updated classes page

--- components/Adverts.vue
+++ components/Adverts.vue
@@ -1,14 +1,14 @@
 <template>
   <v-carousel cycle hide-delimiter-background show-arrows-on-hover>
     <v-carousel-item
-      src="blackFriday.png"
+      src="/blackFriday.png"
       cover
       href="https://voodoocombat.clubright.co.uk/register"
       target="_blank"
     ></v-carousel-item>
 
     <v-carousel-item
-      src="rechargeBanner.png"
+      src="/rechargeBanner.png"
       cover
       href="https://voodoorecharge.co.uk/"
       target="_blank"
