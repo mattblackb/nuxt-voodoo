ce1fdbb47a47093e5e0970e5fcebe74cd9b860c2
mattblackb <87385656+mattblackb@users.noreply.github.com>
2024-06-25 10:23:39 +0100

    Updated closures

--- components/Closures.vue
+++ components/Closures.vue
@@ -2,215 +2,478 @@
   <v-container>
     <v-row>
       <v-col cols="12" sm="12" md="6">
-        <h3>ADULTS CLASS CLOSURES<br />2023 to 2024</h3>
-        <p>
-          <strong>CHRISTMAS HOLIDAY</strong><br />
-          Last Day of Training:<br />
-          Wednesday 20th December 2023<br />
-          <i>
-            <strong>
-              Closed Inclusive:<br />
-              Thursday 21st December 2023<br />
-              to Saturday 2nd January 2024<br />
-            </strong>
-          </i>
-          Training Restarts:<br />
-          Wednesday 3rd January 2024
-        </p>
-        <h3>2024 to 2025</h3>
-        <p>
-          <strong>EASTER BANK HOLIDAY</strong><br />
-          Last Day of Training:<br />
-          Thursday 28th March 2024<br />
-          <em>
-            <strong>
-              Closed Inclusive:<br />
-              Friday 29th March 2024 to Monday 1st April 2024 </strong
-            >s
-          </em>
-          <br />
-          Training Restarts:<br />
-          Tuesday 2nd April 2024
-        </p>
+        <h3>ADULTS CLASS CLOSURES - 2024</h3>
 
         <p>
-          <strong>EARLY MAY BANK HOLIDAY</strong><br />
-          Last Day of Training:<br />
-          Friday 3rd May 2024<br />
-          <em>
-            <strong>
-              Closed Inclusive:<br />
-              Saturday 4th May 2024
-              <br />
-              to Monday 6th May 2024
-            </strong>
-          </em>
-          <br />
-          Training Restarts:<br />
-          Tuesday 2nd April 2024
+          <b>SUMMER BANK HOLIDAY</b>
         </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 23rd August</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 24th August</b>
+        </p>
+        <p>
+          <b>to Monday 26th August</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Tuesday 27th August</p>
 
         <p>
-          <strong>SPRING BANK HOLIDAY</strong><br />
-          Last Day of Training:<br />
-          Friday 24th May 2024<br />
-          <em>
-            <strong>
-              Closed Inclusive:<br />
-              Saturday 25th May 2024<br />
-              to Monday 27th May 2024
-            </strong>
-          </em>
-          <br />
-          Training Restarts:<br />
-          Tuesday 28th May 2024
+          <b>CHRISTMAS HOLIDAY</b>
         </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 20th December</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 21st December</b>
+        </p>
+        <p>
+          <b>to Saturday 4th January 2025</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Sunday 5th January 2025</p>
+
+        <h3>2025</h3>
 
         <p>
-          <strong> SUMMER BANK HOLIDAY</strong><br />
-          Last Day of Training:<br />
-          Friday 23rd August 2024
-          <br />
-          <em>
-            <strong>
-              Closed Inclusive:<br />
-              Saturday 24th August 2024<br />
-              to Monday 26th August 2024
-            </strong>
-          </em>
-          <br />
-          Training Restarts:<br />
-          Tuesday 27th August 2024
+          <b>EASTER BANK HOLIDAY</b>
         </p>
+        <p>Last Day of Training:</p>
+        <p>Thursday 17th April</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Friday 18th April</b>
+        </p>
+        <p>
+          <b>to Monday 21st April</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Tuesday 22nd April</p>
 
         <p>
-          <strong>CHRISTMAS HOLIDAY</strong><br />
-          Last Day of Training:<br />
-          Friday 20th December 2024<br />
-          <em>
-            <strong>
-              Closed Inclusive:<br />
-              Saturday 21st December 2024<br />
-              to Saturday 4th January 2025
-            </strong>
-          </em>
-          <br />
-          Training Restarts:<br />
-          Sunday 5th January 2025
+          <b>EARLY MAY BANK HOLIDAY</b>
         </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 2nd May</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 3rd May</b>
+        </p>
+        <p>
+          <b>to Monday 5th May</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Tuesday 6th May</p>
+
+        <p>
+          <b>SPRING BANK HOLIDAY</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 23rd<sup> </sup>May</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 24th May</b>
+        </p>
+        <p>
+          <b>to Monday 26th May</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Tuesday 27th May</p>
+
+        <p>
+          <b>SUMMER BANK HOLIDAY</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 22nd August</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 23rd August</b>
+        </p>
+        <p>
+          <b>to Monday 25th August</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Tuesday 26th August</p>
+
+        <p>
+          <b>CHRISTMAS HOLIDAY</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 19th December</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 20th December</b>
+        </p>
+        <p>
+          <b>to Saturday 3rd January 2026</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Sunday 4th January 2026</p>
+
+        <h3>2026</h3>
+
+        <p>
+          <b>EASTER BANK HOLIDAY</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Thursday 2nd April</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Friday 3rd April</b>
+        </p>
+        <p>
+          <b>to Monday 6th April</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Tuesday 7th April</p>
+
+        <p>
+          <b>EARLY MAY BANK HOLIDAY</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 1st May</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 2nd May</b>
+        </p>
+        <p>
+          <b>to Monday 4th May</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Tuesday 5th May</p>
+
+        <p>
+          <b>SPRING BANK HOLIDAY</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 22nd<sup> </sup>May</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 23rd May</b>
+        </p>
+        <p>
+          <b>to Monday 25th May</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Tuesday 26th May</p>
+
+        <p>
+          <b>SUMMER BANK HOLIDAY</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 28th August</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 29th August</b>
+        </p>
+        <p>
+          <b>to Monday 31st August</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Tuesday 1st<sup> </sup>September</p>
+
+        <p>
+          <b>CHRISTMAS HOLIDAY</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Tuesday 22nd December</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Wednesday 23rd December</b>
+        </p>
+        <p>
+          <b>to Saturday 2nd January 2027</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Sunday 3rd January 2027</p>
       </v-col>
 
       <v-col cols="12" sm="12" md="6">
-        <h3>KIDS CLASS CLOSURES<br />2023 to 2024</h3>
-        <p>
-          <strong>CHRISTMAS HOLIDAY</strong><br />
-          Last Day of Training:<br />
-          Wednesday 20th December 2023<br />
-          <i>
-            <strong>
-              Closed Inclusive:<br />
-              Thursday 21st December 2023<br />
-              to Saturday 2nd January 2024<br />
-            </strong>
-          </i>
-          Training Restarts:<br />
-          Wednesday 3rd January 2024
-        </p>
-        <h3>2024 to 2025</h3>
-        <p>
-          <strong>SPRING HALF TERM</strong><br />
-          Last Day of Training:<br />
-          Friday 16th February 2024<br />
-          <em>
-            <strong>
-              Closed Inclusive:<br />
-              Saturday 17th February 2024 <br />to Saturday 24th February 2023
-            </strong>
-          </em>
-          <br />
-          Training Restarts:<br />
-          Sunday 25th February 2023
-        </p>
+        <h3>KIDS CLASS CLOSURES 2024</h3>
 
         <p>
-          <strong>SPRING BREAK / EASTER</strong><br />
-          Last Day of Training:<br />
-          Wednesday 27th March 2024<br />
-          <em>
-            <strong>
-              Closed Inclusive:<br />
-              Thursday 28th March 2024<br />
-              to Saturday 13th April 2024
-            </strong>
-          </em>
-          <br />
-          Training Restarts:<br />
-          Sunday 14th April 2024
+          <b>SUMMER HOLIDAY</b>
         </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 19th July</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 20th July</b>
+        </p>
+        <p>
+          <b>to Saturday 31st August</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Sunday 1st September</p>
 
         <p>
-          <strong> EARLY MAY BANK HOLIDAY</strong><br />
-          Last Day of Training:<br />
-          Friday 3rd May 2024<br />
-          <em>
-            <strong>
-              Closed Inclusive:<br />
-              Saturday 25th May 2024<br />
-              to Saturday 1st June 2024
-            </strong>
-          </em>
-          <br />
-          Training Restarts:<br />
-          Sunday 2nd June 2024
+          <b>AUTUMN HALF TERM / HALLOWEEN</b>
         </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 25th October</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 26th October</b>
+        </p>
+        <p>
+          <b>to Saturday 2nd November</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Sunday 3rd November</p>
 
         <p>
-          <strong> SUMMER HOLIDAY</strong><br />
-          Last Day of Training:<br />
-          Friday 19th July 2024
-          <br />
-          <em>
-            <strong>
-              Closed Inclusive:<br />
-              Saturday 20th July 2024<br />
-              to Saturday 31st August 2024
-            </strong>
-          </em>
-          <br />
-          Training Restarts:<br />
-          Sunday 1st September 2024
+          <b>CHRISTMAS HOLIDAY</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Thursday 19th December</p>
+        <p>
+          <b>Closed Inclusive:</b>
         </p>
         <p>
-          <strong> AUTUMN HALF TERM / HALLOWEEN</strong><br />
-          Last Day of Training:<br />
-          Thursday 24th October 2024
-          <br />
-          <em>
-            <strong>
-              Closed Inclusive:<br />
-              Friday 25th October 2024<br />
-              to Saturday 1st November 2024
-            </strong>
-          </em>
-          <br />
-          Training Restarts:<br />
-          Sunday 2nd November 2024
+          <b>Friday 20th December</b>
         </p>
         <p>
-          <strong>CHRISTMAS HOLIDAY</strong><br />
-          Last Day of Training:<br />
-          Thursday 19th December 2024<br />
-          <em>
-            <strong>
-              Closed Inclusive:<br />
-              Friday 20th December 2024<br />
-              to Saturday 4th January 2025
-            </strong>
-          </em>
-          <br />
-          Training Restarts:<br />
-          Sunday 5th January 2025
+          <b>to Saturday 4th January 2025</b>
         </p>
+        <p>Training Restarts:</p>
+        <p>Sunday 5th January 2025</p>
+
+        <h3>2025</h3>
+
+        <p>
+          <b>SPRING HALF TERM</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 14th February</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 15th February</b>
+        </p>
+        <p>
+          <b>to Saturday 22nd February</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Sunday 23rd February</p>
+
+        <p>
+          <b>SPRING BREAK / EASTER</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 4th April</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 5th April</b>
+        </p>
+        <p>
+          <b>to Monday 21st April</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Tuesday 22nd April</p>
+
+        <p>
+          <b>EARLY MAY BANK HOLIDAY</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 2nd May</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 3rd May</b>
+        </p>
+        <p>
+          <b>to Monday 5th May</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Tuesday 6th May</p>
+
+        <p>
+          <b>SUMMER HALF TERM</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 23rd May</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 24th May</b>
+        </p>
+        <p>
+          <b>to Saturday 31st May</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Sunday 1st June</p>
+
+        <p>
+          <b>SUMMER HOLIDAY</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 18th July</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 19th July</b>
+        </p>
+        <p>
+          <b>to Saturday 30th August</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Sunday 31st August</p>
+
+        <p>
+          <b>AUTUMN HALF TERM / HALLOWEEN</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 24th October</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 25th October</b>
+        </p>
+        <p>
+          <b>to Saturday 1st November</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Sunday 2nd November</p>
+
+        <p>
+          <b>CHRISTMAS HOLIDAY</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Thursday 18th December</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Friday 19th December</b>
+        </p>
+        <p>
+          <b>to Saturday 3rd January 2026</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Sunday 4th January 2026</p>
+
+        <h3>2026</h3>
+
+        <p>
+          <b>SPRING HALF TERM</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 13th February</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 14th February</b>
+        </p>
+        <p>
+          <b>to Saturday 21st February</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Sunday 22nd February</p>
+
+        <p>
+          <b>SPRING BREAK / EASTER</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 27th March</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 28th March</b>
+        </p>
+        <p>
+          <b>to Saturday 11th April</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Sunday 12th April</p>
+
+        <p>
+          <b>EARLY MAY BANK HOLIDAY</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 1st May</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 2nd May</b>
+        </p>
+        <p>
+          <b>to Monday 4th May</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Tuesday 5th May</p>
+
+        <p>
+          <b>SUMMER HALF TERM</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 22nd May</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 23rd May</b>
+        </p>
+        <p>
+          <b>to Saturday 30th May</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Sunday 31st May</p>
+
+        <p>
+          <b>SUMMER HOLIDAY</b>
+        </p>
+        <p>Last Day of Training:</p>
+        <p>Friday 17th July</p>
+        <p>
+          <b>Closed Inclusive:</b>
+        </p>
+        <p>
+          <b>Saturday 18th July</b>
+        </p>
+        <p>
+          <b>to Monday 31st August</b>
+        </p>
+        <p>Training Restarts:</p>
+        <p>Tuesday 1st September</p>
       </v-col>
     </v-row>
   </v-container>
